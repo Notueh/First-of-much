975d192923cf851bd7bf1a848b72f37d14051cf0
Heuton <84808505+Notueh@users.noreply.github.com>
2021-05-26 18:37:25 -0300

    Add files via upload

---
+++ Questao  4 Vetor.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <locale.h>
+
+main(){
+// para escrever correto no codigo
+    setlocale(LC_ALL,"");
+
+    int vetor[14];
+    int i, X, Y, soma, aux;
+// para preencher o vetor
+    printf("Preencha o valores para cada posição abaixo\n");
+    for( i = 0; i < 15; i++){
+        printf("vetor[%i]: ", i);
+        scanf("%i", &vetor[i]);
+    }
+// para informar e validar as posições
+    do {
+        printf("Informe a posição X: ");
+        scanf("%i", &X);
+        printf("Informe a posição Y: ");
+        scanf("%i", &Y);
+
+        if((X < 0 || X >= 15)||(Y < 0 || Y >= 15)) {
+        printf("Posições Invalidas!\n");
+        aux = 1;
+        }else {
+        aux = 0;
+        }
+    }while (aux == 1);
+
+    soma = vetor[X] + vetor[Y];
+
+    printf("O valor contido na posição X é %i.\n", vetor[X]);
+    printf("O valor contido na posição Y é %i.\n", vetor[Y]);
+    printf("A soma dos valores contidos nas posições X e Y do vetor é %i", soma);
+
+}
