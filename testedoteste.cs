d5763344a05f65d392cc6b1771daaf7d65206823
Heuton <84808505+Notueh@users.noreply.github.com>
2021-05-27 20:31:37 -0300

    Add files via upload

---
+++ testedoteste.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teste
+{
+    class testedoteste
+    {
+        public double calcular Salario(String tipoFuncionario, String cargo) {
+            if (tipoFuncionario.Equals("desenvolvedor")) {
+                if(carg.Equals("fullstack1")) {
+                    return 2500,00;
+                }
+                if (carg.equals("fullstack2")) {
+                    return 4500,00;
+                }
+                if (carg.equals("fullstack3")) {
+                    return 6500,00;
+                }
+            }
+            if (tipoFuncionario.Equals("gerente")) {
+                return 12000,00
+            }
+    }
+}
